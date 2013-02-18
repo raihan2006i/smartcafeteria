1d39bfca40fc065c7805bdffaf7bb19f52866c96
Raihan Mahmud Arman <raihan2006i@gmail.com>
2013-02-18 02:01:31 +0100

    Updated UI

--- assets/js/charts/area.js
+++ assets/js/charts/area.js
@@ -1,42 +1,33 @@
 $(function () {
-	// we use an inline data source in the example, usually data would
-	// be fetched from a server
-	var data = [], totalPoints = 200;
-	function getRandomData() {
-		if (data.length > 0)
-			data = data.slice(1);
+  // we use an inline data source in the example, usually data would
+  // be fetched from a server
+  var data_month = [ ["January", 10000], ["February", 8000], ["March", 12000], ["April", 24000], ["May", 17000], ["June", 19000] ];
+  var data_date = [ ["01/02/2013", 1400], ["02/02/2013", 1800], ["03/02/2013", 1500], ["04/02/2013", 1200], ["05/02/2013", 1100], ["06/02/2013", 1300] ];
+  $.plot("#calorie-chart-by-month", [ data_month ], {
+    series: {
+      bars: {
+        show: true,
+        barWidth: 0.6,
+        align: "center"
+      }
+    },
+    xaxis: {
+      mode: "categories",
+      tickLength: 0
+    }
+  });
 
-		while (data.length < totalPoints) {
-			var prev = data.length > 0 ? data[data.length - 1] : 50;
-			var y = prev + Math.random() * 10 - 5;
-			if (y < 0)
-				y = 0;
-			if (y > 100)
-				y = 100;
-			data.push(y);
-		}
-
-		var res = [];
-		for (var i = 0; i < data.length; ++i)
-			res.push([i, data[i]])
-		return res;
-	}
-
-	// setup plot
-	var options = {
-		yaxis: { min: 0, max: 100 },
-		xaxis: { min: 0, max: 100 },
-		colors: ["#F90", "#222", "#666", "#BBB"],
-		series: {
-				   lines: { 
-						lineWidth: 2, 
-						fill: true,
-						fillColor: { colors: [ { opacity: 0.6 }, { opacity: 0.2 } ] },
-						steps: false
-
-					}
-			   }
-	};
-	
-	var plot = $.plot($("#area-chart"), [ getRandomData() ], options);
+  $.plot("#calorie-chart-by-date", [ data_date ], {
+    series: {
+      bars: {
+        show: true,
+        barWidth: 0.6,
+        align: "center"
+      }
+    },
+    xaxis: {
+      mode: "categories",
+      tickLength: 0
+    }
+  });
 });
